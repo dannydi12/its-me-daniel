0354ba4918c2f5b36ab4d30b657bfe91ef9d53a6
Daniel Di Venere <danieldivenere@gmail.com>
2020-01-24 19:40:52 -0800

    about styling

--- src/About/About.js
+++ src/About/About.js
@@ -9,8 +9,8 @@ function About() {
         {/* <h2>About Me</h2> */}
         <h2 className='title'>I'm a web developer.</h2>
         <p>I spend my time building new websites and tools while simultaneously willing quirky side-projects into existence. I also have an unhealthy obsession with Elon Musk, I tend to impulsively learn new things for no apparent reason, and I think Indie music is pretty cool.</p>
-        <p className='underline'><br></br>Things I speak:</p>
-        <WordList words={['HTML5', 'CSS3', 'Javascript', 'jQuery', 'React', 'Git', 'AJAX', 'Node', 'Postgres']} />
+        <p className='test'><br></br>Things I speak:</p>
+        <WordList words={['HTML5', 'CSS3', 'Javascript', 'jQuery', 'React', 'Git', 'AJAX', 'Node', 'Postgres', 'Python']} />
         <p>But don't worry, I also speak human languages like English and French.</p>
       </div>
     </section>
