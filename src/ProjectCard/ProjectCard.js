bdc4c2a8d5c48d83161bd4deaff858641a2e7a98
Daniel Di Venere <danieldivenere@gmail.com>
2020-01-24 20:34:55 -0800

    progress in styling projects

--- src/ProjectCard/ProjectCard.js
+++ src/ProjectCard/ProjectCard.js
@@ -1,20 +1,17 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import './ProjectCard.css';
-import WordList from '../WordList/WordList';
-import widthContext from '../widthContext';
 
 function ProjectCard(props) {
-  const width = useContext(widthContext);
   return (
     <div className='project-card'>
       <img className='project-image' src={props.project.image} alt={props.project.title + ' landing page'} />
       <div className='project-wrapper'>
         <h5 className='project-title'>{props.project.title}</h5>
         <p>{props.project.description}</p>
-        <WordList words={props.project.languages} />
+        <p>{props.project.languages.map((language, i) => <span key={i} className='language'>{language}{i+1 === props.project.languages.length ? '' : ', '}</span>)}</p>
         <div className='project-links'>
-          <a className='underline' target='_blank' rel="noopener noreferrer" href={props.project.live}>Live</a>
-          <a className='underline' target='_blank' rel="noopener noreferrer" href={props.project.repo}>Repo</a>
+          <a target='_blank' rel="noopener noreferrer" href={props.project.live}>Live</a>
+          <a target='_blank' rel="noopener noreferrer" href={props.project.repo}>Repo</a>
         </div>
       </div>
     </div>
