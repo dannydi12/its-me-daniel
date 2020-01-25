b2a1f5a61c57e54e39ea614fd0f01a8553b0094e
Daniel Di Venere <danieldivenere@gmail.com>
2020-01-24 17:16:34 -0800

    componentized about

--- src/About/About.js
+++ src/About/About.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './About.css';
+import WordList from '../WordList/WordList';
 
 function About() {
-  return(
-    <section>
-
+  return (
+    <section className='about-section'>
+      <div className='section-wrapper'>
+        {/* <h2>About Me</h2> */}
+        <h2 className='title'>I'm a web developer.</h2>
+        <p>I spend my time building new websites and tools while simultaneously willing quirky side-projects into existence. I also have an unhealthy obsession with Elon Musk, I tend to impulsively learn new things for no apparent reason, and I think Indie music is pretty cool.</p>
+        <p className='underline'><br></br>Things I speak:</p>
+        <WordList words={['HTML5', 'CSS3', 'Javascript', 'jQuery', 'React', 'Git', 'AJAX', 'Node', 'Postgres']} />
+        <p>But don't worry, I also speak human languages like English and French.</p>
+      </div>
     </section>
   );
 }
--- src/App.js
+++ src/App.js
@@ -4,21 +4,13 @@ import './App.css';
 import widthContext from './widthContext';
 import WordList from './WordList/WordList';
 import Landing from './Landing/Landing';
+import About from './About/About';
 
 function App() {
   return (
     <main>
       <Landing />
-      <section className='about-section'>
-        <div className='wrapper'>
-          {/* <h2>About Me</h2> */}
-          <h2 className='title'>I'm a web developer.</h2>
-          <p>I spend my time building new websites and tools while simultaneously willing quirky side-projects into existence. I also have an unhealthy obsession with Elon Musk, I tend to impulsively learn new things for no apparent reason, and I think Indie music is pretty cool.</p>
-          <p className='underline'><br></br>Things I speak:</p>
-          <WordList words={['HTML5', 'CSS3', 'Javascript', 'jQuery', 'React', 'Git', 'AJAX', 'Node', 'Postgres']} />
-          <p>But don't worry, I also speak human languages like English and French.</p>
-        </div>
-      </section>
+      <About />
       <section className='projects-section'>
 
       </section>
