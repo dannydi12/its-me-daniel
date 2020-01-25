93455b221f3f07846cb4a90e8f3233e703bcb34e
Daniel Di Venere <danieldivenere@gmail.com>
2020-01-25 10:54:09 -0800

    added code block to about section

--- src/About/About.js
+++ src/About/About.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './About.css';
 import WordList from '../WordList/WordList';
+import Highlight from 'react-highlight';
+import './tomorrow-night-bright.css';
+
 
 function About() {
   return (
@@ -9,8 +12,26 @@ function About() {
         {/* <h2>About Me</h2> */}
         <h2 className='title'>I'm a web developer.</h2>
         <p>I spend my time building new websites and tools while simultaneously willing quirky side-projects into existence. I also have an unhealthy obsession with Elon Musk, I tend to impulsively learn new things for no apparent reason, and I think Indie music is pretty cool.</p>
-        <p className='test'><br></br>Things I speak:</p>
-        <WordList words={['HTML5', 'CSS3', 'Javascript', 'jQuery', 'React', 'Git', 'AJAX', 'Node', 'Postgres', 'Python']} />
+        <p className='test'><br></br>aboutMe.json:</p>
+        {/* <WordList words={['HTML5', 'CSS3', 'Javascript', 'jQuery', 'React', 'Git', 'AJAX', 'Node', 'Postgres', 'Python']} /> */}
+
+        <div className='my-code'>
+          <Highlight className="json">
+            {`{
+  "things I speak": [
+    "HTML5",
+    "CSS3",
+    "Javascript",
+    "jQuery",
+    "React",
+    "Git",
+    "AJAX",
+    "Node",
+    "Postgres"
+  ]
+}`}
+          </Highlight>
+        </div>
         <p>But don't worry, I also speak human languages like English and French.</p>
       </div>
     </section>
