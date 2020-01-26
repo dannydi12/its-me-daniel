0c27064b19b9decaf94721d3f14f3ac056c2ba6e
Daniel Di Venere <danieldivenere@gmail.com>
2020-01-26 00:00:37 -0800

    added links

--- src/Attribution/Attribution.js
+++ src/Attribution/Attribution.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Attribution.css';
-import { Link, withRouter } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Attribution(props) {
   return (
@@ -11,7 +11,9 @@ function Attribution(props) {
         </header>
         <blockquote>"If I have seen further it is by standing on the shoulders of Giants."<br />- Isaac Newton, 1675</blockquote>
         <ul>
-          <li>Avatar made by <a href='#'>Avataaar.com</a></li>
+          <li>Avatar made by <a href='https://getavataaars.com/' target='_blank' rel="noopener noreferrer">Avataaar.com</a></li>
+          <li>Fonts used: <a href='https://fonts.google.com/specimen/Poppins' target='_blank' rel="noopener noreferrer">Poppins</a> and <a href='https://www.cufonfonts.com/font/segoe-ui-4' target='_blank' rel="noopener noreferrer">Segoe UI</a></li>
+          <li>Code block style by <a href='https://github.com/akiran/react-highlight' target='_blank' rel="noopener noreferrer">Akiran</a></li>
         </ul>
         <Link className='back' to='/'>Back</Link>
       </div>
@@ -19,4 +21,4 @@ function Attribution(props) {
   );
 }
 
-export default withRouter(Attribution);
+export default Attribution;
