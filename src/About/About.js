83584dc1aafa6073c8c3b0b28040b78be8bffec5
Daniel Di Venere <danieldivenere@gmail.com>
2020-01-25 21:09:57 -0800

    renamed title and cleaned up

--- src/About/About.js
+++ src/About/About.js
@@ -9,12 +9,10 @@ function About() {
   return (
     <section className='about-section'>
       <div className='section-wrapper'>
-        {/* <h2>About Me</h2> */}
         <h2 className='title'>I'm a web developer.</h2>
         <p>I spend my time building new websites and tools while simultaneously willing quirky side-projects into existence. I also have an unhealthy obsession with Elon Musk, I tend to impulsively learn new things for no apparent reason, and I think Indie music is pretty cool.</p>
-        <p className='test'><br></br>aboutMe.json:</p>
+        <p className='test'><br></br>languages.json:</p>
         {/* <WordList words={['HTML5', 'CSS3', 'Javascript', 'jQuery', 'React', 'Git', 'AJAX', 'Node', 'Postgres', 'Python']} /> */}
-
         <div className='my-code'>
           <Highlight className="json">
             {`{
