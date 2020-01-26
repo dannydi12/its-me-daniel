2c1ffd935960be59ca9065ffc5befb43a196a624
Daniel Di Venere <danieldivenere@gmail.com>
2020-01-25 19:53:42 -0800

    finished contact section

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -12,13 +12,17 @@ function Contact() {
         </header>
         <TimeBasedMesage />
         <ul>
-          <li><a href='#'>Github</a></li>
-          <li><a href='#'>LinkedIn</a></li>
-          <li><a href='#'>Email</a></li>
+          <li><a href='https://github.com/dannydi12/' target='_blank' rel="noopener noreferrer">Github</a></li>
+          <li><a href='https://www.linkedin.com/in/danieldivenere/' target='_blank' rel="noopener noreferrer">LinkedIn</a></li>
+          <li><a href="mailto:danieldivenere@gmail.com?subject=Hey,%20let's%20talk.&body=What's%20the%20meaning%20of%20Life,%20the%20Universe,%20and%20Everything?" target='_blank' rel="noopener noreferrer">Email</a></li>
         </ul>
+        <p>If traditional methods of communication aren't your thing, click the button and it will turn on the custom LED system I installed in my room. It's pretty jarring, and I'll just think it's the robot uprising.</p>
+        <button>Scare Daniel</button>
       </div>
     </section>
   );
 }
 
 export default Contact;
+
+// What's%20the%20meaning%20of%20Life,%20the%20Universe,%20and%20Everything?\n\nWarmly,\nYour%20Bestest%20Friend
