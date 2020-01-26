8ae1d24c0c297bf0193768c4d80752695ed8b45b
Daniel Di Venere <danieldivenere@gmail.com>
2020-01-26 15:41:27 -0800

    made cursor blink in object

--- src/About/About.js
+++ src/About/About.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './About.css';
+import './tomorrow-night-bright.css';
 import WordList from '../WordList/WordList';
 import Highlight from 'react-highlight';
-import './tomorrow-night-bright.css';
 
 
 function About() {
@@ -11,7 +11,7 @@ function About() {
       <div className='section-wrapper'>
         <h2 className='title'>I'm a web developer.</h2>
         <p>I spend my time building new websites and tools while simultaneously willing quirky side-projects into existence. I also have an unhealthy obsession with Elon Musk, I tend to impulsively learn new things for no apparent reason, and I think Indie music is pretty cool.</p>
-        <p className='test'><br></br>languages.json:</p>
+        <p className='underline'><br></br>languages.json:</p>
         {/* <WordList words={['HTML5', 'CSS3', 'Javascript', 'jQuery', 'React', 'Git', 'AJAX', 'Node', 'Postgres', 'Python']} /> */}
         <div className='my-code'>
           <Highlight className="json">
@@ -25,9 +25,12 @@ function About() {
     "Git",
     "AJAX",
     "Node",
-    "Postgres"
+    "Postgres",
+    `}
+            "<span className="blinking-cursor">|</span>"
+  {`
   ]
-}`}
+}` }
           </Highlight>
         </div>
         <p>But don't worry, I also speak human languages like English and French.</p>
