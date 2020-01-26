ddb9ad30114f04c70fc3eedf49f4ac5979308b62
Daniel Di Venere <danieldivenere@gmail.com>
2020-01-25 23:27:14 -0800

    attribution style and structure established

--- src/Attribution/Attribution.js
+++ src/Attribution/Attribution.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './Attribution.css';
+import { Link, withRouter } from 'react-router-dom'
 
-function Attribution() {
-  return(
-    <main>
-
+function Attribution(props) {
+  return (
+    <main className='attribution'>
+      <div className='attribution-container'>
+        <header>
+          <h1>Attribution</h1>
+        </header>
+        <blockquote>"If I have seen further it is by standing on the shoulders of Giants."<br />- Isaac Newton, 1675</blockquote>
+        <ul>
+          <li>Avatar made by <a href='#'>Avataaar.com</a></li>
+        </ul>
+        <Link className='back' to='/'>Back</Link>
+      </div>
     </main>
   );
 }
 
-export default Attribution;
+export default withRouter(Attribution);
