0e51ce75a588bc3fd9c10285b6891a5543c6c2d3
Daniel Di Venere <danieldivenere@gmail.com>
2020-01-25 21:41:20 -0800

    made avatar interactive

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Landing.css';
 import Avatar from 'avataaars';
 
 function Landing() {
+  const [expression, setExpression] = useState({
+    eyes: 'Happy',
+    mouth: 'Smile'
+  });
   return (
-    <header className='landing'>
+    <header
+      className='landing'
+      onClick={() => setExpression({
+        eyes: expression.eyes === 'Happy' ? 'Surprised' : 'Happy',
+        mouth: expression.mouth === 'Smile' ? 'ScreamOpen' : 'Smile'
+      })}
+      onMouseEnter={() => setExpression({
+        eyes: expression.eyes === 'Happy' ? 'Surprised' : 'Happy',
+        mouth: expression.mouth === 'Smile' ? 'ScreamOpen' : 'Smile'
+      })}
+      onMouseLeave={() => setExpression({
+        eyes: expression.eyes === 'Happy' ? 'Surprised' : 'Happy',
+        mouth: expression.mouth === 'Smile' ? 'ScreamOpen' : 'Smile'
+      })}>
       <div className='landing-container'>
         <h1>Hi, it's me. Daniel.</h1>
         {/* <p>I'm a web developer</p> */}
@@ -17,9 +34,9 @@ function Landing() {
         facialHairType='Blank'
         clotheType='Hoodie'
         clotheColor='Blue03'
-        eyeType='Happy'
+        eyeType={expression.eyes}
         eyebrowType='DefaultNatural'
-        mouthType='Smile'
+        mouthType={expression.mouth}
         skinColor='Light'
       />
     </header>
