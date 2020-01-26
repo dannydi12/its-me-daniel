31bae60857680ece5016b0f9ca35d0f623b6366e
Daniel Di Venere <danieldivenere@gmail.com>
2020-01-25 18:20:09 -0800

    componentized timebasedmessage

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,46 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './Contact.css';
-import moment from 'moment';
+import TimeBasedMesage from '../TimeBasedMessage/TimeBasedMessage';
 
 function Contact() {
-  const [time, updateTime] = useState(moment().format('h:mma'));
-
-  useEffect(() => {
-    console.log(time);
-  }, [time]);
-
-  useEffect(() => {
-    const interval = setInterval(() => updateTime(moment().format('h:mma')), 1000);
-    return () => clearInterval(interval);
-  }, []);
-
-  const message = () => {
-    const hour = moment().hour();
-    const minute = moment().minute();
-
-    switch (true) {
-      case (moment().format('h:mm') === '11:11'):
-        return (<p>It's {time}, which means I'm probably wishing for it to be 11:12{moment().format('a')}</p>);
-
-      case (hour >= 7 && hour < 9):
-        return (<p>It's {time}, which means I'm probably on a hike. Let's walk and talk.</p>);
-
-      case (hour >= 9 && hour < 17):
-        return (<p>It's {time}, which means I'm probably designing a website right now.<br /> Reach out so we can collaborate or just talk code.</p>);
-
-      case (hour >= 17 && hour < 20):
-        return (<p>It's {time}, which means I'm probably thinking about how I can help get humanity to Mars. Want to help?</p>);
-
-      case (hour >= 20 && hour < 22):
-        return (<p>It's {time}, which means I'm probably making music. Reach out so we can jam. Or talk code. Or both?</p>);
-
-      case (hour >= 22 && minute <= 30):
-        return (<p>It's {time}, which means I'm probably watching Rick and Morty. Let's talk in dimension C-137 near the FlimFlorp.</p>);
-
-      case (hour >= 22 && hour < 7):
-        return (<p>It's {time}, which means I'm probably dreaming about elon musk. Is that you Elon? If so, use this email: xxxxx</p>);
-    }
-  }
 
   return (
     <section className='contact-section'>
@@ -48,7 +10,7 @@ function Contact() {
         <header>
           <h2>Contact</h2>
         </header>
-        {message()}
+        <TimeBasedMesage />
       </div>
     </section>
   );
---
+++ src/TimeBasedMessage/TimeBasedMessage.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+
+function TimeBasedMesage() {
+  const [time, updateTime] = useState(moment().format('h:mma'));
+
+  // useEffect(() => {
+  //   console.log(time);
+  // }, [time]);
+
+  useEffect(() => {
+    const interval = setInterval(() => updateTime(moment().format('h:mma')), 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const message = () => {
+    const hour = moment().hour();
+    const minute = moment().minute();
+
+    switch (true) {
+      case (moment().format('h:mm') === '11:11'):
+        return (<p>It's {time}, which means I'm probably wishing for it to be 11:12{moment().format('a')}</p>);
+
+      case (hour >= 7 && hour < 9):
+        return (<p>It's {time}, which means I'm probably on a hike. Let's walk and talk.</p>);
+
+      case (hour >= 9 && hour < 17):
+        return (<p>It's {time}, which means I'm probably designing a website right now.<br /> Reach out so we can collaborate or just talk code.</p>);
+
+      case (hour >= 17 && hour < 20):
+        return (<p>It's {time}, which means I'm probably thinking about how I can help get humanity to Mars. Want to help?</p>);
+
+      case (hour >= 20 && hour < 22):
+        return (<p>It's {time}, which means I'm probably making music. Reach out so we can jam. Or talk code. Or both?</p>);
+
+      case (hour >= 22 && minute <= 30):
+        return (<p>It's {time}, which means I'm probably watching Rick and Morty. Let's talk in dimension C-137 near the FlimFlorp.</p>);
+
+      case (hour >= 22 && hour < 7):
+        return (<p>It's {time}, which means I'm probably dreaming about elon musk. Is that you Elon? If so, use this email: xxxxx</p>);
+
+      default:
+        return (<p>It's {time}, which means I'm probably designing a website right now.<br /> Reach out so we can collaborate or just talk code.</p>);
+    }
+  }
+
+  return (
+    <>
+      {message()}
+    </>
+  );
+}
+
+export default TimeBasedMesage;
