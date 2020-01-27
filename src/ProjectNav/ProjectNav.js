1e2d83cf034d3fd7ff244a84aba19677eb1fcf74
Daniel Di Venere <danieldivenere@gmail.com>
2020-01-26 16:07:49 -0800

    made project nav more readable

--- src/ProjectNav/ProjectNav.js
+++ src/ProjectNav/ProjectNav.js
@@ -4,7 +4,10 @@ import './ProjectNav.css';
 function ProjectNav(props) {
   const categories = props.categories.map((category, i) => (
     <li key={i}>
-      <button onClick={() => props.update(category)}>{props.currentCategory === category ? <span className='b-text nav-underline'>{category}</span> : <span className='b-text'>{category}</span>}</button>
+      <button onClick={() => props.update(category)}>
+        {props.currentCategory === category ?
+          <span className='b-text nav-underline'>{category}</span> : <span className='b-text'>{category}</span>}
+      </button>
     </li>
   ));
   return (
