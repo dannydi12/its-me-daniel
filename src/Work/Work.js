aaa9f47384859ffaab72cf9c004a6f6ca0f78331
Daniel Di Venere <danieldivenere@gmail.com>
2020-01-27 14:39:39 -0800

    added animation for projects

--- src/Work/Work.js
+++ src/Work/Work.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Work.css';
 import ProjectCard from '../ProjectCard/ProjectCard';
 import ProjectNav from '../ProjectNav/ProjectNav';
+import Fade from 'react-reveal/Fade';
 import STORE from './ProjectsStore';
 
 function Work() {
@@ -9,16 +10,18 @@ function Work() {
 
   const allProjects = STORE.filter(project => project.category.find(category => category === categoryState));
   const filteredProjects = allProjects.map((project, i) => <ProjectCard key={i} project={project} />);
-  
+
   return (
     <section className='work-section'>
       <div className='section-wrapper'>
         <header>
           <h2>Work</h2>
-          <ProjectNav categories={['Featured', 'Freelance']} update={setCategoryState} currentCategory={categoryState}/>
+          <ProjectNav categories={['Featured', 'Freelance']} update={setCategoryState} currentCategory={categoryState} />
         </header>
         <div className='projects'>
-          {filteredProjects}
+          <Fade duration={2000} spy={filteredProjects}>
+            {filteredProjects}
+          </Fade>
         </div>
       </div>
     </section>
