221a1bdb48e3c26d1b9e1908b547adb0903be3ee
Daniel Di Venere <danieldivenere@gmail.com>
2020-05-30 09:10:27 -0700

    added npmm to projects section

--- src/Work/ProjectsStore.js
+++ src/Work/ProjectsStore.js
@@ -1,5 +1,14 @@
 const STORE = [
   {
+    title: 'Pi LED API',
+    category: ['Other'],
+    description: 'A turn-key solution for setting up remotely-controlled WS281x LEDs on a Raspberry Pi with Express. Also includes a React front end for ease of use.',
+    image: '/images/led.png',
+    languages: ['Python', 'Node', 'Express', 'React', 'HTML', 'CSS', 'Javascript', 'AJAX', 'Mocha', 'Chai', 'Jest'],
+    repo: 'https://github.com/dannydi12/pi-led-server',
+    live: 'https://github.com/dannydi12/pi-led-client#screenshots',
+  },
+  {
     title: 'Tunezilla',
     category: ['Other'],
     description: 'A simplistic lyric-fetching web app that optionally provides background and historical information about your favorite songs.',
@@ -18,6 +27,15 @@ const STORE = [
     live: 'https://dannydi12.github.io/mocking-text-generator/',
   },
   {
+    title: 'NPMM',
+    category: ['Featured'],
+    description: 'An open source project that allows developers to save their favorite packages and install them with NPMM\'s companion CLI.',
+    image: '/images/npmm.png',
+    languages: ['React', 'Redux', 'Node', 'PostgreSQL', 'Express', 'AJAX', 'Mocha', 'Chai'],
+    repo: 'https://github.com/dannydi12/npmm-client',
+    live: 'https://npmm.dev',
+  },
+  {
     title: 'ThoughtBin',
     category: ['Featured'],
     description: 'A frictionless micro-platform that allows users to anonymously share and create content. Eliminates emphasis on followers, likes, or reputation.',
@@ -25,15 +43,6 @@ const STORE = [
     languages: ['React', 'Node', 'PostgreSQL', 'Express', 'WebSockets', 'AJAX', 'Mocha', 'Chai'],
     repo: 'https://github.com/dannydi12/thoughtbin-client',
     live: 'https://thoughtbin.imdan.io',
-  },
-  {
-    title: 'Pi LED API',
-    category: ['Featured'],
-    description: 'A turn-key solution for setting up remotely-controlled WS281x LEDs on a Raspberry Pi with Express. Also includes a React front end for ease of use.',
-    image: '/images/led.png',
-    languages: ['Python', 'Node', 'Express', 'React', 'HTML', 'CSS', 'Javascript', 'AJAX', 'Mocha', 'Chai', 'Jest'],
-    repo: 'https://github.com/dannydi12/pi-led-server',
-    live: 'https://github.com/dannydi12/pi-led-client#screenshots',
   },
   {
     title: 'Chocolate Milk Media',
