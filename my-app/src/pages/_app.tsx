29338aca0d14482c7ce3006f7564b0e907515ea7
Daniel Di Venere <danieldivenere@gmail.com>
2023-04-08 17:11:09 -0700

    sass was horrible

--- my-app/src/pages/_app.tsx
+++ my-app/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.scss";
+import "@/styles/globals.css";
 import "@/styles/tomorrow-night-bright.css";
 import type { AppProps } from "next/app";
 
--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -1,14 +1,9 @@
-import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "next/font/google";
 import Landing from "../components/Landing/Landing";
 import About from "../components/About/About";
 import Work from "../components/Work/Work";
 import Contact from "../components/Contact/Contact";
 import Footer from "../components/Footer/Footer";
-import "../styles/home.module.scss";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
