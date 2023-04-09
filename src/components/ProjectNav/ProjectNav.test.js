48cebfa46996cc9910cdb23780cff9c22c301d38
Daniel Di Venere <danieldivenere@gmail.com>
2023-04-08 19:23:10 -0700

    remove tests

--- src/components/ProjectNav/ProjectNav.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ProjectNav from './ProjectNav';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<ProjectNav categories={['Featured', 'Freelance']} update={() => {}} currentCategory="Featured" />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
