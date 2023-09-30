78de50533e595adf4e80293e79bb9a11bcf80e93
Daniel Di Venere <danieldivenere@gmail.com>
2023-09-29 21:41:20 -0700

    move script tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,17 +6,6 @@ const Document: FC = () => {
   return (
     <Html lang="en">
       <Head>
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-QLGVG137CN" />
-        <Script id="google-analytics">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', 'G-QLGVG137CN');
-        `}
-        </Script>
-
         <title>Daniel DiVenere | Portfolio</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -71,6 +60,16 @@ const Document: FC = () => {
         />
       </Head>
       <body>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-QLGVG137CN" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-QLGVG137CN');
+        `}
+        </Script>
         <Main />
         <NextScript />
       </body>
