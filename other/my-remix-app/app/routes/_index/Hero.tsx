23657e447b8cc283aa9fd4b66adcc8940bf347e0
Daniel Di Venere <danieldivenere@gmail.com>
2024-03-17 14:08:55 -0700

    hero is complete!

--- other/my-remix-app/app/routes/_index/Hero.tsx
+++ other/my-remix-app/app/routes/_index/Hero.tsx
@@ -1,9 +1,27 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import { useState } from "react";
+
 export default function Hero() {
+  const [interacted, setInteracted] = useState(false);
+
   return (
     <section className="bg-primary-500 min-h-[50vh] flex items-center flex-col justify-between">
       <h1 className="text-white text-8xl font-bold my-auto text-center">
         Hi, it&apos;s me. Daniel.
       </h1>
+
+      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
+      <div
+        onMouseEnter={() => setInteracted(!interacted)}
+        onMouseLeave={() => setInteracted(!interacted)}
+        onClick={() => setInteracted(!interacted)}
+      >
+        <img
+          className="max-w-min"
+          src={interacted ? "/assets/scared.svg" : "/assets/happy.svg"}
+          alt="me"
+        />
+      </div>
     </section>
   );
 }
