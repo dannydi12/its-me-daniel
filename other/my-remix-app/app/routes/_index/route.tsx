d5d16139bd6495ece6d495b9f9c5c36cffb6915d
Daniel Di Venere <danieldivenere@gmail.com>
2024-03-17 12:33:12 -0700

    scaffolding sections

---
+++ other/my-remix-app/app/routes/_index/route.tsx
@@ -0,0 +1,18 @@
+import type { MetaFunction } from "@remix-run/node";
+import Hero from "./Hero";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <main>
+      <h1>Welcome to Remix</h1>
+      <Hero />
+    </main>
+  );
+}
--- other/my-remix-app/tailwind.config.ts
+++ other/my-remix-app/tailwind.config.ts
@@ -1,8 +1,15 @@
 import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      ...colors,
+      primary: {
+        500: "#a29bfe",
+      },
+    },
     extend: {},
   },
   plugins: [],
