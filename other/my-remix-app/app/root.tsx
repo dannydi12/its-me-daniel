4e7c35243ab2ac320fca8bbc9576c6f0dbfbf313
Daniel Di Venere <danieldivenere@gmail.com>
2024-03-17 14:41:50 -0700

    rename tailwind file

--- other/my-remix-app/app/root.tsx
+++ other/my-remix-app/app/root.tsx
@@ -6,7 +6,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import stylesheet from "./tailwind.css?url";
+import stylesheet from "./index.css?url";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
