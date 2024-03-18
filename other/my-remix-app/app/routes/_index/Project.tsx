93f39d1ec6985c8f2abe8d23dfe8b6d8dda0c6d2
Daniel Di Venere <danieldivenere@gmail.com>
2024-03-17 18:23:01 -0700

    complete project section

---
+++ other/my-remix-app/app/routes/_index/Project.tsx
@@ -0,0 +1,45 @@
+import { Link } from "@remix-run/react";
+
+type Props = {
+  title: string;
+  description: string;
+  image: string;
+  languages: string[];
+  repo?: string;
+  live: string;
+};
+export default function Project({
+  title,
+  description,
+  image,
+  languages,
+  repo,
+  live,
+}: Props) {
+  return (
+    <div className="h-fit max-w-[355px] mt-8 mb-12 shadow-md mx-auto">
+      <img className="w-full h-[185px] max-w-[355px]" src={image} alt={title} />
+      <div className="p-5">
+        <h3 className="text-[1.7rem] font-bold mb-3">{title}</h3>
+        <p>{description}</p>
+        <p className="text-gray-500 my-5">{languages.join(", ")}</p>
+        <div className="flex justify-around">
+          <Link
+            className="text-primary-500 border-[3px] p-2 text-lg w-[30%] text-center border-primary-500 hover:bg-primary-500 hover:text-white"
+            to={live}
+          >
+            Live
+          </Link>
+          {repo && (
+            <Link
+              className="text-primary-500 border-[3px] p-2 text-lg w-[30%] text-center border-primary-500 hover:bg-primary-500 hover:text-white"
+              to={repo}
+            >
+              Repo
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
--- other/my-remix-app/app/routes/_index/Work.tsx
+++ other/my-remix-app/app/routes/_index/Work.tsx
@@ -1,6 +1,7 @@
 import STORE from "./project-store";
 import { useSearchParams } from "@remix-run/react";
 import ProjectNav from "./ProjectNav";
+import Project from "./Project";
 
 export default function Work() {
   const [searchParams] = useSearchParams();
@@ -17,6 +18,20 @@ export default function Work() {
       <h2 className="my-10 text-center font-semibold text-4xl">Work</h2>
 
       <ProjectNav />
+
+      <div className="flex flex-wrap">
+        {allProjects.map((project) => (
+          <Project
+            key={project.title}
+            title={project.title}
+            description={project.description}
+            image={project.image}
+            languages={project.languages}
+            live={project.live}
+            repo={project.repo}
+          />
+        ))}
+      </div>
     </section>
   );
 }
