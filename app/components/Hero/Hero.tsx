95088985479d8f0bb85c25d196fc67a99d9f9c99
Daniel Di Venere <danieldivenere@gmail.com>
2024-03-17 22:17:48 -0700

    fix large header on mobile

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
 
   return (
     <section className="bg-primary-500 min-h-[50vh] flex items-center flex-col justify-between">
-      <h1 className="text-white text-8xl font-bold my-auto text-center">
+      <h1 className="text-white text-4xl sm:text-8xl font-bold my-auto text-center">
         Hi, it&apos;s me. Daniel.
       </h1>
 
