703d1958729ca71a14e07600600db1b6db7f4247
Daniel Di Venere <danieldivenere@gmail.com>
2024-03-22 23:32:11 -0700

    markdown parsing and restructuring

--- app/routes/blog.$slug/getMarkdownFilePath.tsx
+++ app/routes/blog.$slug/getMarkdownFilePath.tsx
@@ -1,15 +1,27 @@
 import path from "path";
 import { fileURLToPath } from "url";
 
-export const getMarkdownFilePath = (filename: string) => {
+export const getMarkdownFilePath = (
+  filename: string,
+  addExtension?: boolean
+) => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
 
   const absolutePath = path.resolve(
     __dirname,
     "../../../blog",
-    `${filename}.md`
+    `${filename}${addExtension ? ".md" : ""}`
   );
+
+  return absolutePath;
+};
+
+export const getFolderPath = () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const absolutePath = path.resolve(__dirname, "../../../blog");
 
   return absolutePath;
 };
--- app/routes/blog.$slug/markdownParsing.tsx
+++ app/routes/blog.$slug/markdownParsing.tsx
@@ -1,4 +1,6 @@
 import { unified } from "unified";
+// @ts-expect-error no-implicit-any -- this library does not have a type definition
+import preview, { textFormatter } from "remark-preview";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkParseFrontmatter from "remark-parse-frontmatter";
 import remarkParse from "remark-parse";
@@ -9,14 +11,19 @@ import rehypeSlug from "rehype-slug";
 import rehypeExternalLinks from "rehype-external-links";
 import { readingTime } from "hast-util-reading-time";
 import { read } from "to-vfile";
+import { getFolderPath, getMarkdownFilePath } from "./getMarkdownFilePath";
 
-export const parseMarkdown = async (filePath: string) => {
-  function inferReadingTime() {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any -- can't find the types for these 🤷‍♂️
-    return (tree: any, file: any) => {
-      file.data.frontmatter.readTime = readingTime(tree, { age: 16 });
-    };
-  }
+import fs from "fs";
+
+const inferReadingTime = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any -- can't find the types for these 🤷‍♂️
+  return (tree: any, file: any) => {
+    file.data.frontmatter.readTime = readingTime(tree, { age: 16 });
+  };
+};
+
+export const parseMarkdown = async (slug: string) => {
+  const filePath = getMarkdownFilePath(slug, true);
 
   const result = await unified()
     .use(remarkParse)
@@ -31,4 +38,52 @@ export const parseMarkdown = async (filePath: string) => {
     .process(await read(filePath));
 
   return result;
+};
+
+export const buildPostList = async () => {
+  // get path to /blog folder of markdown files
+  const folderPath = getFolderPath();
+
+  // read all filenames from the top-level of the /blog folder
+  const filenames = fs.readdirSync(folderPath);
+
+  // only keep the markdown files
+  const markdownFilenames = filenames.filter((filename) =>
+    filename.match(".md")
+  );
+
+  const posts = markdownFilenames.map(async (filename) => {
+    // generate absolute path to markdown file
+    const path = getMarkdownFilePath(filename);
+
+    const result = await unified()
+      .use(remarkParse)
+      .use(remarkFrontmatter, ["yaml", "toml"])
+      .use(remarkParseFrontmatter)
+      .use(
+        preview(
+          textFormatter({
+            length: 200,
+          })
+        )
+      )
+      .use(remarkRehype)
+      .use(inferReadingTime)
+      .use(rehypeStringify)
+      .process(await read(path));
+
+    const frontmatter = result.data.frontmatter as {
+      share: boolean;
+      slug: string;
+      title: string;
+      readTime: number;
+    };
+
+    const previewData = result.data.fm as { preview: string[] };
+    const previewText = previewData.preview;
+
+    return { ...frontmatter, preview: previewText };
+  });
+
+  return await Promise.all(posts);
 };
