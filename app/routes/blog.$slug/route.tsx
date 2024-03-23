3b0f1ef761e1b4e41e36b3ed33124d35c2b110c5
Daniel Di Venere <danieldivenere@gmail.com>
2024-03-22 20:28:03 -0700

    footer

--- app/routes/blog.$slug/route.tsx
+++ app/routes/blog.$slug/route.tsx
@@ -1,6 +1,6 @@
 import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import { getMarkdownFilePath } from "./getMarkdownFilePath";
 import { parseMarkdown } from "./markdownParsing";
 
@@ -33,7 +33,7 @@ export default function PostSlug() {
   return (
     <>
       <div className="flex min-h-[30vh] items-center justify-center bg-primary-500 p-2 py-7 sm:p-20">
-        <h1 className="text-center text-5xl font-bold text-white drop-shadow sm:text-7xl sm:mt-0 mt-10">
+        <h1 className="mt-10 text-center text-5xl font-bold text-white drop-shadow sm:mt-0 sm:text-7xl">
           {meta.title}
         </h1>
       </div>
@@ -41,6 +41,18 @@ export default function PostSlug() {
         className="prose m-5 pt-12 sm:prose-lg prose-h2:mb-5 prose-h2:text-4xl prose-p:text-gray-950 prose-a:text-primary-600 prose-code:rounded-md prose-code:bg-gray-200 prose-code:px-2 prose-code:py-1 prose-code:before:content-none prose-code:after:content-none prose-pre:bg-transparent prose-pre:p-0 prose-img:mx-auto prose-img:rounded-md sm:mx-auto prose-h2:sm:text-5xl "
         dangerouslySetInnerHTML={{ __html: content }}
       />
+
+      <footer className="mt-[15vh] bg-primary-500 font-medium text-white">
+        <p className="py-4 text-center text-lg">
+          Crafted by{" "}
+          <Link className="underline" to={"/"}>
+            Daniel Di Venere
+          </Link>{" "}
+          <span role="img" aria-label="ice cream">
+            🤖
+          </span>
+        </p>
+      </footer>
     </>
   );
 }
