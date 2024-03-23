96700d423c687c31756dbbed809c1505213abfb3
Daniel Di Venere <danieldivenere@gmail.com>
2024-03-22 23:29:09 -0700

    addd blog link

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import scaredSVG from "assets/scared.svg?url";
 import happySVG from "assets/happy.svg?url";
+import { Link } from "@remix-run/react";
 
 export default function Hero() {
   const [interacted, setInteracted] = useState(false);
 
   return (
     <section className="flex min-h-[50vh] flex-col items-center justify-between bg-primary-500">
+      <div className="m-8 flex self-end">
+        <Link
+          className="hover:cursor flex items-center gap-1 rounded px-4 py-2 text-xl text-white backdrop-brightness-75 transition-all hover:backdrop-brightness-90"
+          to={"/blog"}
+        >
+          Blog
+        </Link>
+      </div>
       <h1 className="my-auto text-center text-4xl font-bold text-white sm:text-8xl">
         Hi, it&apos;s me. Daniel.
       </h1>
