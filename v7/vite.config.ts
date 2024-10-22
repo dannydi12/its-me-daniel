b1502317aff2e10fa9ea6cddaeda2306c15f76ff
Daniel Di Venere <danieldivenere@gmail.com>
2024-10-22 15:22:40 -0700

    an absolute failure

---
+++ v7/vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import { reactRouter } from "@react-router/dev/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRouter()],
+})
